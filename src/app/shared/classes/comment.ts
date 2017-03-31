916f89278d41dcbadfeb247d5bb11ef92f54eb74
SergeyKolenko <venty80@gmail.com>
2017-03-31 16:15:53 +0300

    added classes

--- src/app/shared/classes/comment.ts
+++ src/app/shared/classes/comment.ts
@@ -1,2 +1,12 @@
+import { User } from './user';
+
 export class Comment {
+  id?: number;
+  user: User;
+  text: string;
+
+  constructor(user: User, text: string) {
+    this.user = user;
+    this.text = text;
+  }
 }
--- src/app/shared/classes/post.ts
+++ src/app/shared/classes/post.ts
@@ -1,2 +1,14 @@
+import { Comment } from './comment';
+import { User } from './user';
+
 export class Post {
+  id?: number;
+  body: string;
+  comments?: Comment[];
+  user: User;
+
+  constructor(body: string, user: User) {
+    this.body = body;
+    this.user = user;
+  }
 }
