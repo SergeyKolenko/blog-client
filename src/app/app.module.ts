6c69cc993127602d4c478bd2aff80de32303241d
SergeyKolenko <venty80@gmail.com>
2017-03-31 14:18:49 +0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './posts/post/post.component';
+import { CommentsComponent } from './comments/comments.component';
+import { AlertModule, ModalModule } from 'ng2-bootstrap';
+import { PostService } from './shared/services/post.service';
+import { CommentService } from './shared/services/comment.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule.forRoot(),
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [PostService, CommentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
