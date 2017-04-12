8e5b6c1b845eabd010c72b4051ace94a99a3d7ba
SergeyKolenko <venty80@gmail.com>
2017-04-12 20:05:10 +0300

    added guard

--- src/app/posts/posts.routes.ts
+++ src/app/posts/posts.routes.ts
@@ -1,7 +1,8 @@
-import  { Route } from '@angular/router';
+import { Route } from '@angular/router';
 import { PostComponent } from './post/post.component';
 import { PostNewComponent } from './post-new/post-new.component';
 import { PostsComponent } from './posts.component';
+import { Angular2TokenService } from 'angular2-token';
 
 export const PostsRouter: Route[] = [
   {
@@ -19,5 +20,10 @@ export const PostsRouter: Route[] = [
   {
     path: 'posts/:id',
     component: PostComponent
+  },
+  {
+    path: 'posts/:id/edit',
+    component: PostComponent,
+    canActivate: [Angular2TokenService]
   }
 ];
